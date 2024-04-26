a10e13239d58ace6ab64f7c1c7502a6b1836fb2e
ShaimaaMonem <monem@mpi-magdeburg.mpg.de>
2024-04-26 14:35:06 +0200

    debug mode test

--- main.py
+++ main.py
@@ -8,9 +8,10 @@ from external.splocs.view_animation import view_anim_file
 from external.splocs.view_splocs import view_components
 from functools import partial
 from IPDGS.config.config import vertPos_numFrames, snapshots_format, frame_increament, input_snapshots_pattern, \
-                                input_animation_dir, snapshots_animation_file, \
-                                visualize_snapshots, vertPos_output_directory, vertPos_numComponents, aligned_snapshots_directory, aligned_snapshots_animation_file, \
-                                vertPos_output_animation_file, snapshots_anim_ready, rigid
+                                input_animation_dir, snapshots_animation_file, show_profile, \
+                                visualize_snapshots, vertPos_output_directory, vertPos_numComponents,\
+                                aligned_snapshots_directory, aligned_snapshots_animation_file, \
+                                vertPos_output_animation_file, snapshots_anim_ready, rigid, visualize_bases, store_bases
 
 
 root_folder = os.getcwd()
@@ -52,7 +53,8 @@ def main():
         print("A snapshots file already exists: \n", aligned_snapshots_h5_file,
               "\n .. we skip import! ")
 
-    if visualize_snapshots == "Yes":
+    # see aligned snapshots
+    if visualize_snapshots:
         view_anim_file(aligned_snapshots_h5_file)
 
     # read and pre-process snapshots
@@ -64,25 +66,23 @@ def main():
 
     # store bases
     bases.store_animations(vertPos_output_directory)
-    # bases.store_components_to_files(vertPos_output_directory, vertPos_numComponents, vertPos_numComponents, 10, '.npy')
-    # bases.store_components_to_files(vertPos_output_directory, 10, vertPos_numComponents, 10, '.bin')
 
-    view_components(os.path.join(vertPos_output_directory, bases.output_components_file))
+    # store .bin and .npy format for bases for use and comparision
+    if store_bases:
+        bases.store_components_to_files(vertPos_output_directory, vertPos_numComponents, vertPos_numComponents, 10, '.npy')
+        bases.store_components_to_files(vertPos_output_directory, 10, vertPos_numComponents, 10, '.bin')
+
+    if visualize_bases:
+        view_components(os.path.join(vertPos_output_directory, bases.output_components_file))
+
+
 if __name__ == '__main__':
 
-    # parser = argparse.ArgumentParser(
-    # 	description='Find Deformation Bases')
-    # parser.add_argument('input_animation_file')
-    # parser.add_argument('output_components_file')
-    # parser.add_argument('-a', '--output_anim',
-    # 					help='Output animation file (will also save the component weights)')
-    # args = parser.parse_args()
-    # main(args.input_animation_file,
-    # 	args.output_components_file,
-    # 	args.output_anim)
-
-    profiler.enable()
-    main()
-    profiler.disable()
-    stats = pstats.Stats(profiler).sort_stats('tottime')
-    # stats.print_stats()
+    if show_profile:
+        profiler.enable()
+        main()
+        profiler.disable()
+        stats = pstats.Stats(profiler).sort_stats('tottime')
+        stats.print_stats()
+    else:
+        main()
